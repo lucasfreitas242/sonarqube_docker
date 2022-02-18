5731330be412350568eb23e5d8b5c3612d9d540f
Lucas Freitas <lucas@xsys.com.br>
2022-02-18 14:53:03 -0300

    inserindo códigos para teste

--- source.js
+++ source.js
@@ -6,6 +6,16 @@ oper = readlineSync.question("Qual operacao deseja efetuar (+) (-) (*) (/)? : \n
 valor = parseFloat(readlineSync.question("Insira o primeiro numero: \n"));
 valor1 = parseFloat(readlineSync.question("Insira o segundo numero: \n"));
 
+x = 13;
+
+let haj = false;
+
+if(false) {
+    return 12;
+}
+
+
+
 function doOperation(operator, value1, value2) {
     if (operator == "+") {
         return value1 + value2;
